c94d8a9c55db803213a067ef4ccc85637331300b
Virag Kuvadia <viragkuvadia.25@gmail.com>
2016-04-10 10:57:50 +0530

    changes : virag

--- app/src/main/java/app/glintcarwash/com/glintapp/OrderActivity.java
+++ app/src/main/java/app/glintcarwash/com/glintapp/OrderActivity.java
@@ -1,5 +1,6 @@
 package app.glintcarwash.com.glintapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,7 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class OrderActivity extends Fragment {
+public class OrderActivity extends Fragment implements View.OnClickListener {
     ImageView imgCar,imgOrder;
 
 
@@ -18,14 +19,22 @@ public class OrderActivity extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.activity_order,container,false);
-        imgOrder = (ImageView) v.findViewById(R.id.imageView1);
-        imgCar = (ImageView) v.findViewById(R.id.imageView2);
+        imgOrder = (ImageView) v.findViewById(R.id.imgOrder);
+        imgCar = (ImageView) v.findViewById(R.id.imgCar);
+        imgOrder.setOnClickListener(this);
+        imgCar.setOnClickListener(this);
         return v;
     }
 
-    public  void book_car(View v){
-        Intent color_picker = new Intent(getActivity(), BookOrderActivity.class);
-        startActivity(color_picker);
-    }
 
+    @Override
+    public void onClick(View v) {
+        if(v==imgCar){
+            Intent color_picker = new Intent(getActivity(), BookOrderActivity.class);
+            startActivity(color_picker);
+        }else if(v==imgOrder){
+            Intent color_picker = new Intent(getActivity(), BookOrderActivity.class);
+            startActivity(color_picker);
+        }
+    }
 }
