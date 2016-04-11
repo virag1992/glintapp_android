c82c8cd85daa8dea9cd8687adfcc762dde85513f
Virag Kuvadia <viragkuvadia.25@gmail.com>
2016-04-11 23:33:08 +0530

    Notification list added

--- app/src/main/java/app/glintcarwash/com/glintapp/NotificationFragment.java
+++ app/src/main/java/app/glintcarwash/com/glintapp/NotificationFragment.java
@@ -1,7 +1,9 @@
 package app.glintcarwash.com.glintapp;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
@@ -11,6 +13,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +29,7 @@ import global.ProgressWheel;
 public class NotificationFragment extends Fragment {
     Dialog DialogForTransaction;
     ProgressWheel progressWheel;
+    ListView lstNotification;
 
     ArrayList<Integer> m_draw = new ArrayList<Integer>();
 
@@ -37,7 +43,11 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.activity_splash, null);
+        View v = inflater.inflate(R.layout.notification_list,container,false);
+        lstNotification = (ListView) v.findViewById(R.id.lstNotification);
+        ListViewCustomAdapter adapter = new ListViewCustomAdapter(getActivity());
+        lstNotification.setAdapter(adapter);
+        return v;
     }
 
     @Override
@@ -48,6 +58,73 @@ public class NotificationFragment extends Fragment {
         m_draw.add(Color.parseColor("#FFB347"));
         m_draw.add(Color.parseColor("#03C03C"));
         m_draw.add(Color.parseColor("#FDFD96"));
+
+    }
+
+    public class ListViewCustomAdapter extends BaseAdapter {
+        public Activity context;
+        LayoutInflater inflater;
+
+        public ListViewCustomAdapter(Activity context)
+
+        {
+            super();
+            this.context = context;
+            this.inflater = (LayoutInflater) context
+                    .getSystemService(context.LAYOUT_INFLATER_SERVICE);
+
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return 15;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public class Holder {
+            TextView title1;
+            TextView title2;
+            RelativeLayout rlMain;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            // TODO Auto-generated method stub
+
+            Holder hv;
+
+            if (arg1 == null) {
+                hv = new Holder();
+                arg1 = inflater.inflate(R.layout.notification_custom_item, null);
+                hv.title1 = (TextView) arg1.findViewById(R.id.txtTile1);
+                hv.title2 = (TextView) arg1.findViewById(R.id.txtTile2);
+                hv.rlMain = (RelativeLayout) arg1.findViewById(R.id.rlMain);
+                arg1.setTag(hv);
+            } else {
+                hv = (Holder) arg1.getTag();
+            }
+
+            hv.rlMain.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            return arg1;
+        }
 
     }
 
